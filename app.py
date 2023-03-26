e229dd43933ae5ae8cc221ffbd63317e53c5f429
sohalsdr <sohalsdr@gmail.com>
2023-03-26 05:56:04 -0400

    Added word complexity formatting using Aenigma

--- app.py
+++ app.py
@@ -12,10 +12,11 @@
 
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
+from aenigma import analyzer
 
-#---------------------------------#
-#   INITIALIZE FLASK APP AND API  #
-#---------------------------------#
+#-------------------------------------------#
+#   INITIALIZE FLASK APP, API, AND AENIGMA  #
+#-------------------------------------------#
 
 app = Flask(__name__)                               # Creates Flask application
 api = Api(app)                                      # Initializes the API
@@ -29,11 +30,28 @@ class Analyze(Resource):                            # Analysis Endpoint Class
         parser = reqparse.RequestParser()           # Initialize Parser
         parser.add_argument('text', required=True)  # Expects Argument with Text For Analysis
         args = parser.parse_args()                  # Parse Arguments to Dictionary
-        return {'formatted': args['text']}          # Return Formatted Text
+        formatted = format_complexity(args['text']) # Call format_complexity to add complexity stratification
+        return {'formatted': formatted}             # Return Formatted Text
 api.add_resource(Analyze, '/analyze')               # Adds endpoint to Flask API
+
+#--------------------#
+#   HELPER METHODS   #
+#--------------------#
+
+def format_complexity(text: str) -> str:            # Sentence Complexity Formatting Helper
+    rcs_vals = analyzer.generate_rcs_list(text)     # Calls Aenigma to get RCS scores of sentences
+    for (item, rcs) in rcs_vals:                    # Iterate through original text and add spans (preserves whitespace)
+        lvl = int((1-rcs)*6)                        # Level 1 is most emphasized/complex, 6 is least
+        text = text.replace(                        # Replace sentence in text with formatted sentence
+            item,
+            f"<span class='level{lvl}'>{item}</span>"
+            )
+    return text                                     # Return formatted text
 
 #------------------------#
 #   RUNS THE FLASK APP   #
 #------------------------#
 
-if __name__ == '__main__': app.run()                # Dunder
+if __name__ == '__main__':
+    analyzer.initialize_nltk()                      # Downloads tokenizer for Aenigma 
+    app.run()                                       # Runs Flask app
